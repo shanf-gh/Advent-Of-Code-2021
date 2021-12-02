c8aef4e1337688e2fbc39938aeaea2b7fdc508c3
shanf-gh <shan.fabien@gmail.com>
2021-12-03 00:31:16 +1300

    AoC2021 - Initial commit with D1/D2 already done

---
+++ C#/Day 1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Day_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String[] input = System.IO.File.ReadAllLines("input.txt");
+            
+            int[] depth = new int[input.Length];
+            for(int i=0; i < input.Length; i++) {
+                depth[i] = Convert.ToInt32(input[i]);
+            }
+
+            int count = 0;
+
+            for(int i=1; i < depth.Length; i++) {
+                count += depth[i] - depth[i - 1] > 0 ? 1 : 0;
+            }
+
+            Console.WriteLine($"Solution 1: {count}");
+
+            count = 0;
+
+            for(int i=0; i < depth.Length -3; i++) {
+                
+                count += depth[i + 3] - depth[i] > 0 ? 1 : 0;
+            }
+            
+            Console.WriteLine($"Solution 2: {count}");
+        }
+    }
+}
---
+++ C#/Day 2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Day_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String[] input = System.IO.File.ReadAllLines("input.txt");
+
+            int horizontalPos = 0;
+            int verticalPos = 0;
+            int aim = 0;
+            int i = 0;
+
+            foreach(string row in input){
+                string[] rowData = row.Split(" ");
+                int value = Convert.ToInt32(rowData[1]);
+
+                switch(rowData[0]) {
+                    case "forward":
+                        horizontalPos += value;
+                        verticalPos += aim * value;
+                        break;
+                    case "up":
+                        // verticalPos -= value;
+                        aim -= value;
+                        break;
+                    case "down":
+                        // verticalPos += value;
+                        aim += value;
+                        break;
+                }
+            }
+            Console.WriteLine($"Horizontal position is {horizontalPos} and vertical position is {verticalPos}.");
+            Console.WriteLine($"The aim is {aim}");
+            Console.WriteLine($"The product is {horizontalPos * verticalPos}");
+        }
+    }
+}
