d437666261a8bbf2f9455a4c6961d9a4f3e04d77
shanf-gh <shan.fabien@gmail.com>
2021-12-05 00:50:46 +1300

    Completed Day 3 challenge

--- C#/Day 3/Program.cs
+++ C#/Day 3/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Day_3
 {
@@ -10,7 +12,6 @@ namespace Day_3
 
             int[] zeroCount = new int[] {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
             int[] oneCount = new int[] {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-            int j = 0;
 
             foreach(string value in input) {
                 string[] values = value.Split("");
@@ -22,12 +23,6 @@ namespace Day_3
                         zeroCount[i] += 1;
                     }
                 }
-                // Console.WriteLine($"value: {value}");
-
-                // if(j>5) {
-                //     break;
-                // }
-                // j++;
             }
 
             // Determine gamma and epsilon rates
@@ -53,8 +48,59 @@ namespace Day_3
             int powerConsumption = gammaRateDec * epsiloneRateDec;
 
             Console.WriteLine($"Power consumption is: {powerConsumption}");
-                //  Console.WriteLine("[{0}]", string.Join(", ", zeroCount));
-                //  Console.WriteLine("[{0}]", string.Join(", ", oneCount));
+
+            // Step 2 - Life support rating
+
+            // Oxygen generator rating
+            // determine most common binary and split them into different lists
+            List<String> inputList = input.ToList<String>();
+            int oxygenGeneratorRating = Convert.ToInt32(reduce(inputList, 0, "most")[0],2);
+            int CO2ScrubberRating =  Convert.ToInt32(reduce(inputList, 0, "least")[0],2);
+            int lifeSupportRating = oxygenGeneratorRating * CO2ScrubberRating;
+
+            Console.WriteLine($"oxygenGeneratorRating is: {oxygenGeneratorRating}");
+            Console.WriteLine($"CO2ScrubberRating is: {CO2ScrubberRating}");
+            Console.WriteLine($"Life support rating is: {lifeSupportRating}");
+
+        }
+
+        static List<string> reduce(List<String> currInput, int depth, string type) {
+            List<string> currZero =  new List<string>();
+            List<string> currOne = new List<string>();
+            int zeroCount = 0;
+            int oneCount = 0;
+			Boolean useOneList = true;
+		
+			if(currInput.Count == 1) {
+				// Console.WriteLine("stopped at depth: " + depth);
+				return currInput;
+			}
+
+            foreach(string value in currInput) {
+                char[] values = value.ToCharArray();
+
+                if(values[depth]=='0') {
+                    currZero.Add(value);
+                    zeroCount += 1;
+                }
+
+                if(values[depth]=='1') {
+                    currOne.Add(value);
+                    oneCount += 1;
+                }
+            }
+		
+			if(type == "most") {
+				useOneList = zeroCount <= oneCount;
+			} else if (type == "least") {
+				useOneList = zeroCount > oneCount;
+			}	
+
+            if(useOneList) {
+                return reduce(currOne, depth+1, type);
+            } else {
+                return reduce(currZero, depth+1, type);
+            }		
         }
     }
 }
